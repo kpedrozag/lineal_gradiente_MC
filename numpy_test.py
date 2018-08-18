b2d4c1be67e4e36762877558c9942958670beb4f
KEVIN_PEDROZA <kevin.andres01@hotmail.com>
2018-08-17 21:51:54 -0500

    listo sin probar

---
+++ numpy_test.py
@@ -0,0 +1,14 @@
+import numpy as np 
+a = np.array((10,11,1))
+print("a: ", a)
+
+b = a.copy()
+a[0] = 9999
+
+print("a: ", a)
+print("b: ", b)
+
+
+"""
+
+"""
--- plantilla.py
+++ plantilla.py
@@ -3,42 +3,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
 
-#Load Dataset
+
+# tr: numero de datos usados para entrenamiento (N)
+# ft: features (x)
+# hp: array de thetas. hipotesis (theta)
+# op: outputs (y)
+def error(tr, ft, hp, op):
+    e = 0.0
+    for i in range(tr, len(ft)):
+        e += (np.dot(hp, ft[i]) - op[i])**2
+    return e
+
+# Load Dataset
 dataSet=np.loadtxt("ex1data2.txt",delimiter=',')
-x=dataSet[:,[0,1]]
-y=dataSet[:,2]
+x=dataSet[:,[0,1]] # features
+y=dataSet[:,2] # outputs
 
-tetha = np.zeros(3)
-m = len(tetha)
-alfa = 0.001
-N = int(len(x)*0.8)
-test = len(x) - N
+theta = np.zeros(3) # array de theta
+m = len(theta) # tamaño del vector theta
+alfa = 0.001 # alpha: define convergencia del algoritmo
+N = int(len(x)*0.8) # Cantidad de inputs para entrenamiento
+# test = len(x) - N # cantidad de inputs para testing
 
-for q in range(1000):
+for q in range(1000): # 1000 iteraciones
+    theta_temp = np.zeros(3) # array de thetas temporal, se reinicia en 0 al inicio de cada iteracion
+    for j in range(m): # calculo de cada theta
+        sumatoria_hx_y_x = 0 # sumatoria(*) de 0 hasta N datos de entrada
+        for n in range(N): # calculo de la sumatoria(*)
+            sumatoria_hx_y_x += (np.dot(theta, x[n]) - y[n]) * x[n][j] # proceso de la sumatoria
+        theta_temp[j] = theta[j] - ((alfa / N) * sumatoria_hx_y_x) # theta temporal
+    theta = theta_temp.copy() # actualizacion de theta
 
-    for j in range(m):
-
-        sumatoria_hx_y_x = 0
-
-        for n in range(N):
-
-            suma_hx = 0
-            for i in range(m):
-                suma_hx += tetha * x[n][i]
-
-            sumatoria_hx_y_x += (suma_hx - y[n]) * x[n][j]
-        t_temp = tetha[j] - ((alfa / N) * sumatoria_hx_y_x)
-
-
-
-
-
-
-
-
-
-
-
+print("Con la hipotesis:", theta, "se obtiene un error de:\n", error(N, x, theta, y), end='\n\n')
 
 #Create a 3d plot
 fig = plt.figure(figsize=(7,7))
